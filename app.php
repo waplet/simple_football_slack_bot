b2b176f9a29ac439d7dd3c1d7c08480e8d4bc52c
waplet <waplet@gmail.com>
2017-12-07 16:51:39 +0200

    Done done

--- app.php
+++ app.php
@@ -33,7 +33,7 @@ try {
     echo $e->getMessage();
     die;
 }
-error_log(print_r($response, true));
+// error_log(print_r($response, true));
 if (!is_null($response)) {
     if ($response instanceof \w\Bot\structures\HTTPResponse) {
         echo $response->message;
@@ -56,72 +56,4 @@ if (!is_null($response)) {
         });
     }
 }
-
-// $client->getChannelById(getenv('APP_BOT_CHANNEL'))->then(function (\Slack\Channel $channel) use ($client) {
-//     $client->send('Hello from PHP!', $channel);
-// });
-
-// $client = new Slack\RealTimeClient($loop);
-// $client->setToken(getenv('BOT_TOKEN'));
-// $client->connect()->then(function () use ($client) {
-//     $client->getDMByUserId(getenv('ADMIN'))->then(function ($channel) use ($client) {
-//         $client->postMessage(
-//             $client->getMessageBuilder()
-//                 ->setText('Bot started! ' . date('Y-m-d H:i:s'))
-//                 ->setChannel($channel)
-//                 ->create()
-//         );
-//     });
-// });
-// $messageManager = new \w\Bot\MessageManager($footballState, $client);
-// $privateMessageManager = new \w\Bot\PrivateMessageManager($footballState, $client);
-//
-// $client->on('message', function (\Slack\Payload $data) use ($client, $messageManager, $privateMessageManager) {
-//     var_dump($data);
-//     $message = new \Slack\Message\Message($client, $data->getData());
-//     if (isset($data['subtype']) && $data['subtype'] == 'message_changed') {
-//         // Skip change events;
-//         return;
-//     }
-//
-//     $client->getChannelGroupOrDMByID($message->data['channel'])->then(function (ChannelInterface $channel)  use (
-//         $client,
-//         $messageManager,
-//         $privateMessageManager,
-//         $message
-//     ) {
-//         if ($channel instanceof \Slack\Channel) {
-//             $responseBuilder = $messageManager->parseMessage($message);
-//         } else if ($channel instanceof DirectMessageChannel) {
-//             $responseBuilder = $privateMessageManager->parseMessage($message);
-//         } else {
-//             echo "Exiting... Incorrect channel given!";
-//             return;
-//         }
-//
-//         if (is_null($responseBuilder)) {
-//             return;
-//         }
-//
-//         $response = $responseBuilder->setChannel($channel)->create();
-//         $client->postMessage($response);
-//     });
-// });
-//
-// $client->on('game_started', function (\Slack\Message\Message $message) use ($client, $privateMessageManager) {
-//     $client->getDMByUserId($message->data['user'])->then(function (DirectMessageChannel $channel) use ($client, $privateMessageManager, $message) {
-//         $message = clone $message;
-//         $message->data['text'] = ',list';
-//
-//         $responseBuilder = $privateMessageManager->parseMessage($message);
-//
-//         if (is_null($responseBuilder)) {
-//             return;
-//         }
-//
-//         $response = $responseBuilder->setChannel($channel)->create();
-//         $client->postMessage($response);
-//     });
-// });
-
 $loop->run();
