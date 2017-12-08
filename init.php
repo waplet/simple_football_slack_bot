fb2d1facb42d04dd5496d421a312c6cd53b2d19a
waplet <waplet@gmail.com>
2017-12-08 14:05:28 +0200

    Cleaned

--- init.php
+++ init.php
@@ -4,6 +4,6 @@ define('ROOT', __DIR__);
 require __DIR__ . '/vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
-$dotenv->required('BOT_TOKEN');
+$dotenv->required('APP_BOT_OAUTH_TOKEN');
 
 date_default_timezone_set('Europe/Riga');
